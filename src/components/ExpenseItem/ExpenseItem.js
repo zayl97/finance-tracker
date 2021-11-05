e883150d340cdc50fa9f11b283525de916addb7b
Zayl <kam.plociennik@gmail.com>
2021-11-05 15:29:39 +0100

    Separated date from 'ExpenseItem' component into other component
    Added styling as well

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
+import ExpenseDate from "./ExpenseDate/ExpenseDate";
 
-import './ExpenseItem.css' 
+import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-
-    return (
-        <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2 className="expense-item__description">{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2 className="expense-item__description">{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
